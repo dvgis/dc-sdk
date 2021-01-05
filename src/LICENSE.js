5cd18f5e0a76b730a1582aa451863e6aab723ebd
Caven Chen <cavencj@gmail.com>
2021-01-05 19:40:13 +0800

    升级Cesium

--- src/LICENSE.js
+++ src/LICENSE.js
@@ -10,7 +10,7 @@ console.clear()
 console.log(
   `%c \n DC-SDK \n  %c \n 用数字描绘世界之美  %c \n
     版本:${__VERSION__} - ${__TIME__.substr(0, 10)}
-    Cesium版本：1.76
+    Cesium版本：1.77.0
     开发作者:${__AUTHOR__}
     网站主页: ${__HOME_PAGE__}
     github:${__REPOSITORY__}
