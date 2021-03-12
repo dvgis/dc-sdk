f30c395793deca31bba834ebdbd8241e02dbddf3
Caven Chen <cavencj@gmail.com>
2021-03-12 09:40:26 +0800

    Update AmapImageryProvider.js

--- src/core/imagery/provider/AmapImageryProvider.js
+++ src/core/imagery/provider/AmapImageryProvider.js
@@ -18,7 +18,7 @@ const LOAD_MARK_URL =
   'https://webst{s}.is.autonavi.com/appmaptile?lang=zh_cn&size=1&scale=1&style=8&x={x}&y={y}&z={z}'
 class AmapImageryProvider extends Cesium.UrlTemplateImageryProvider {
   constructor(options = {}) {
-    options['url'] = options.style === 'img' ? IMG_URL options.style === 'load'? LOAD_MARK_URL : ELEC_URL
+    options['url'] = options.style === 'img' ? IMG_URL : options.style === 'load'? LOAD_MARK_URL : ELEC_URL
     options['subdomains'] = options.subdomains || ['01', '02', '03', '04']
     if (options.crs === 'WGS84') {
       options['tilingScheme'] = new AmapMercatorTilingScheme()
