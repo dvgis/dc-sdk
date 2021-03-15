f62b15be4f2747a248e0a1b5b169737c743d777c
Caven Chen <cavencj@gmail.com>
2021-03-15 13:20:01 +0800

    add @dc-modules/roaming

---
+++ modules/roaming/RoamingViewMode.js
@@ -0,0 +1,13 @@
+/**
+ * @Author: Caven
+ * @Date: 2020-08-05 10:51:13
+ */
+
+const RoamingViewMode = {
+  FP: '1',
+  TP: '2',
+  TRACKED: 'track',
+  FREE: 'free'
+}
+
+export default RoamingViewMode
---
+++ modules/roaming/index.js
@@ -0,0 +1,8 @@
+/**
+ * @Author: Caven
+ * @Date: 2020-04-13 09:32:31
+ */
+
+export { default as RoamingViewMode } from './RoamingViewMode'
+export { default as RoamingPath } from './RoamingPath'
+export { default as RoamingController } from './RoamingController'
