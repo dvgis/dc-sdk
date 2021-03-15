ac7172e1a160376792806e28222aa3b1e17a2d0f
Caven Chen <cavencj@gmail.com>
2021-03-15 13:10:05 +0800

    add @dc-modules/copy-right

---
+++ modules/copy-right/index.js
@@ -0,0 +1,30 @@
+/**
+ * @Author: Caven
+ * @Date: 2020-02-12 18:53:12
+ */
+
+// eslint-disable-next-line no-console
+console.clear()
+
+// eslint-disable-next-line no-console
+console.log(
+  `%c \n DC-SDK \n  %c \n 用数字描绘世界之美  %c \n
+    版本:${__VERSION__} - ${__TIME__}
+    Cesium版本：1.79.1
+    开发作者:${__AUTHOR__}
+    网站主页: ${__HOME_PAGE__}
+    github:${__REPOSITORY__}
+    授权信息：授权-永久使用此软件当前版本。 \n
+
+    版权声明:
+    1.框架作为一个基础平台，代码开源，任何个人和机构可以修改、重构，无需经过我方授权。
+    2.任何个人和机构修改框架出现的问题，我方无需负责。
+    3.后期会添加一些行业性的插件和工具，代码会适量开源。
+    4.对于我方发布的程序包，任何个人和机构在遵守下列条件的前提下可以永久免费使用:
+     1)程序包完整引用
+     2)保留此版权信息在控制台输出
+    我方保留对此版权信息的最终解释权。`,
+  'font-size:20px;padding-left:70px;color:#EEB422',
+  'font-size:14px;padding-left:50px;color:#EEB422;font-style:oblique',
+  'font-size:12px;color:#0865ba'
+)
