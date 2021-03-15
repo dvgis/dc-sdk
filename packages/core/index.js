d3996cf51da21ceb2dec37565bd8b1dfe0d3904a
Caven Chen <cavencj@gmail.com>
2021-03-15 13:18:27 +0800

    init framework

---
+++ packages/core/index.js
@@ -0,0 +1,48 @@
+/**
+ * @Author: Caven
+ * @Date: 2021-03-13 13:15:38
+ */
+
+const install = function(DC) {
+  if (!DC) {
+    throw new Error('Missing DC Base Package')
+  }
+
+  /**
+   * start
+   */
+  DC.ready = callback => {
+    try {
+      if (!DC.Initialized) {
+        DC.init(() => {
+          try {
+            const components = require('./src/components.js').default
+            DC.mixin(components)
+            DC.Initialized = true
+            callback && callback()
+          } catch (e) {
+            // eslint-disable-next-line no-console
+            console.error(e)
+            DC.Initialized = false
+          }
+        })
+      } else {
+        callback && callback()
+      }
+    } catch (e) {
+      // eslint-disable-next-line no-console
+      console.error(e)
+      DC.Initialized = false
+      throw e
+    }
+  }
+}
+
+/* istanbul ignore if */
+if (typeof window !== 'undefined' && window.DC) {
+  install(window.DC)
+}
+
+export default {
+  install
+}
---
+++ packages/sdk/index.js
@@ -0,0 +1,8 @@
+/**
+ * @Author: Caven
+ * @Date: 2021-03-13 13:14:09
+ */
+
+// import DcCore from '../core'
+
+export default {}
