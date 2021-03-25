74392ded39f284c7938d0e8a835e13c3204a0b6b
Caven Chen <cavencj@gmail.com>
2021-03-25 22:29:34 +0800

    add turf lib

--- modules/namespace/index.js
+++ modules/namespace/index.js
@@ -11,6 +11,12 @@ const getMapv = () => {
   return DC.Namespace?.mapv
 }
 
+const getTurf = () => {
+  return DC.Namespace?.turf
+}
+
 export const Cesium = getCesium()
 
 export const mapv = getMapv()
+
+export const turf = getTurf()
