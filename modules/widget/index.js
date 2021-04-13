fae0a9e37e6574dd74af1b6cb593545d785845d4
Caven Chen <cavencj@gmail.com>
2021-04-13 13:35:16 +0800

    remove attribution

--- modules/widget/index.js
+++ modules/widget/index.js
@@ -3,7 +3,6 @@
  * @Date:2020-03-05 21:53:35
  */
 
-import Attribution from './type/Attribution'
 import ContextMenu from './type/ContextMenu'
 import LocationBar from './type/LocationBar'
 import MapSplit from './type/MapSplit'
@@ -18,7 +17,6 @@ import LoadingMask from './type/LoadingMask'
 
 export default function createWidgets() {
   return {
-    attribution: new Attribution(),
     popup: new Popup(),
     contextMenu: new ContextMenu(),
     tooltip: new Tooltip(),
