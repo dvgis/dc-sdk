3cba9d4970e73fda8f5a2456d76cba9395e8ab11
Caven Chen <cavencj@gmail.com>
2021-11-02 20:15:34 +0800

    upgrade the cesium

--- modules/copy-right/index.js
+++ modules/copy-right/index.js
@@ -10,7 +10,7 @@ console.clear()
 console.log(
   `%c \n DC-SDK \n  %c \n 用数字描绘世界之美  %c \n
     版本:${__VERSION__} - ${__TIME__}
-    Cesium版本：1.86.1
+    Cesium版本：1.87.0
     开发作者:${__AUTHOR__}
     网站主页: ${__HOME_PAGE__}
     github:${__REPOSITORY__}
