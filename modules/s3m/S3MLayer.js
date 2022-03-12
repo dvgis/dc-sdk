6f01b72eb747d31bfcbfc0313402cac0587ce431
Caven Chen <cavencj@gmail.com>
2022-03-12 11:53:43 +0800

    add s3m layer

--- modules/s3m/S3MLayer.js
+++ modules/s3m/S3MLayer.js
@@ -3,13 +3,46 @@
  * @Date: 2022-02-20 13:46:53
  */
 
-import { Cesium } from '@dc-modules/namespace'
 import State from '@dc-modules/state/State'
 import { Layer } from '@dc-modules/layer'
 import S3MTilesLayer from 's3m-lib/S3MTiles/S3MTilesLayer'
+import ClusterLayer from '../layer/type/ClusterLayer'
 
 class S3MLayer extends Layer {
-  constructor(id, url) {
+  constructor(id, url, options = {}) {
     super(id)
+    this._url = url
+    this._options = options
+  }
+
+  get type() {
+    return Layer.getLayerType('s3m')
+  }
+
+  _onAdd(viewer) {
+    this._viewer = viewer
+    delete this._options['context']
+    delete this._options['url']
+    this._delegate = new S3MTilesLayer({
+      context: this._viewer.scene.context,
+      url: this._url,
+      ...this._options
+    })
+    this._viewer.scene.primitives.add(this._delegate)
+    this._addedHook && this._addedHook()
+    this._state = State.ADDED
+  }
+
+  _onRemove() {
+    if (!this._delegate) {
+      return
+    }
+    this._viewer.scene.primitives.remove(this._delegate)
+    this._removedHook && this._removedHook()
+    this._state = State.REMOVED
   }
 }
+
+Layer.registerType('s3m')
+
+export default S3MLayer
--- packages/s3m/src/components.js
+++ packages/s3m/src/components.js
@@ -3,7 +3,7 @@
  * @Date: 2021-03-13 17:17:19
  */
 
-import S3MLayer from '@dc-modules/mapv/MapvLayer'
+import S3MLayer from '@dc-modules/s3m/S3MLayer'
 
 const components = { S3MLayer }
 
