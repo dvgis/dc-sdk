e491ab382935a1540083652c3857ef18d69db62c
Caven Chen <cavencj@gmail.com>
2023-03-11 21:17:44 +0800

    add examples

--- scripts/build.js
+++ scripts/build.js
@@ -11,11 +11,11 @@ const chalk = require('chalk')
 
 shell.echo(chalk.green('build sdk start'))
 
-let outoutDir = path.resolve(__dirname, '..', 'packages/sdk/dist')
+let outDir = path.resolve(__dirname, '..', 'packages/sdk/dist')
 
-fse.ensureDirSync(outoutDir)
+fse.ensureDirSync(outDir)
 
-fse.emptyDirSync(outoutDir)
+fse.emptyDirSync(outDir)
 
 const pkgs = ['base', 'core', 'chart', 'mapv', 's3m']
 
@@ -27,10 +27,11 @@ pkgs.forEach((item, index) => {
     if (exists) {
       if (item === 'base') {
         for (let i = 0; i < 7; i++) {
+          fse.removeSync(path.join(dist, `${i}.js`))
           fse.removeSync(path.join(dist, `${i}.min.js`))
         }
       }
-      fse.copySync(dist, outoutDir)
+      fse.copySync(dist, outDir)
       shell.echo(chalk.yellow(`copy ${item} success`))
       if (index === count - 1) {
         await shell.echo(chalk.green('build sdk end'))
