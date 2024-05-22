7821af23cb1c4c0df7e7c9e1a281e2315000f482
LizZ <409429916@qq.com>
2024-05-22 16:32:51 +0800

    fix(measure):修改量测中间点计算方式和贴地多边形坐标获取错误

--- src/modules/math/midCartesian.js
+++ src/modules/math/midCartesian.js
@@ -5,8 +5,5 @@
 import { Cesium } from '../../namespace'
 
 export default function midCartesian(start, end) {
-  let c1 = Cesium.Ellipsoid.WGS84.cartesianToCartographic(start)
-  let c2 = Cesium.Ellipsoid.WGS84.cartesianToCartographic(end)
-  let cm = new Cesium.EllipsoidGeodesic(c1, c2).interpolateUsingFraction(0.5)
-  return Cesium.Ellipsoid.WGS84.cartographicToCartesian(cm)
+  return Cesium.Cartesian3.midpoint(start, end, new Cesium.Cartesian3())
 }
